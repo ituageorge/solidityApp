c9b0aca43094ec6a537c39f521a03bd9d50a2618
Osagie <ituaosagie100@gmail.com>
2024-05-15 13:24:35 +0100

    create erc-20 token

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,1 +1,8 @@
-console.log("hello")
+const {ether} = require("hardhat");
+(async () => {
+    const contractArtifact = ethers.getContractFactory("TccToken");
+    const deployed = await (await contractArtifact).deploy()
+
+    const contractAddress = await deployed.getAddress();
+    console.log(`Contract deployed on ${contractAddress} successfully`)
+})();
